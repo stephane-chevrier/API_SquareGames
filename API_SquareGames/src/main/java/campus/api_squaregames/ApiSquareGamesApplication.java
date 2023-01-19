b6bad96122a0dc9fc35fba3965b004420a7b0c01
stephane-chevrier <stephane.chevrier@le-campus-numerique.fr>
2023-01-19 11:19:11 +0100

    version initiale vide

---
+++ API_SquareGames/src/main/java/campus/api_squaregames/ApiSquareGamesApplication.java
@@ -0,0 +1,13 @@
+package campus.api_squaregames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSquareGamesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiSquareGamesApplication.class, args);
+    }
+
+}
---
+++ API_SquareGames/src/test/java/campus/api_squaregames/ApiSquareGamesApplicationTests.java
@@ -0,0 +1,13 @@
+package campus.api_squaregames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSquareGamesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
